b309f2cc3045719e442dc73666a7363a3781259b
Gilles Lenfant <gilles.lenfant@gmail.com>
2011-05-16 17:00:13 +0000

    - Added dutsch translation (thanks to Kim Paulissen)   [glenfant]
    - z3c.autoinclude awareness
      [glenfant]

--- README.txt
+++ README.txt
@@ -1,6 +1,29 @@
-How to install ?
-================
+===========================
+ collective.actions.delete
+===========================
 
-Read docs/Install.txt
-go to portal quickinstaller
-select collective.actions.delete and click install
+Deletion of Plone elements from the folder contents view is submitted to a
+confirmation form.
+
+Requires
+========
+
+Plone 3.x or a later version
+
+Installation
+============
+
+Just add ``collective.actions.delete`` to the ``eggs`` list of the
+``plone.recipe.zope2instance`` part in your zc.buildout
+
+License
+=======
+
+This software is provided under the GPL v3 license.
+
+Credits and support
+===================
+
+* Alter Way
+* Youenn Boussard `youenn.boussard@alterway.fr`_
+* Gilles lenfant `gilles.lenfant@alterway.fr`_
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1.1'
+version = '0.1.3'
 
 setup(name='collective.actions.delete',
       version=version,
@@ -10,10 +10,10 @@ setup(name='collective.actions.delete',
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
+          "Framework :: Plone",
+          "Programming Language :: Python",
+          "Topic :: Software Development :: Libraries :: Python Modules",
+          ],
       keywords='folder delete confirmation',
       author='Ingeniweb',
       author_email='support@ingeniweb.com',
@@ -26,8 +26,10 @@ setup(name='collective.actions.delete',
       install_requires=[
           'setuptools',
           # -*- Extra requirements: -*-
-      ],
+          ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
